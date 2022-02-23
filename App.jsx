953c108695c15220acf44f4273c94dc272718814
AndriP8 <andrikcoc26@gmail.com>
2022-02-23 17:23:30 +0700

    chore: add react to project

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import reactDom from "react-dom";
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+reactDom.render(<App />, document.getElementById("root"));
