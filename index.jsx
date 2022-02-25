38c5509c3008bff684281cfd859c88e027edfe74
AndriP8 <andrikcoc26@gmail.com>
2022-02-25 13:54:03 +0700

    refactor: rename file main component

--- index.jsx
+++ index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import reactDom from "react-dom";
-// import "./index.css";
 
 const App = () => {
   return (
     <div>
-      <h1 className="title">Hello world</h1>
+      <h1>Hello world</h1>
     </div>
   );
 };
