dca51b2075afb0393da64bf0d5511a2ea3883320
AndriP8 <andrikcoc26@gmail.com>
2022-02-25 13:54:41 +0700

    chore: update cofig webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,25 @@ const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "./App.jsx"),
+  entry: path.resolve(__dirname, "./index.jsx"),
   output: {
     path: path.resolve(__dirname, "./build"),
-    filename: "app.js",
+    filename: "index.js",
     clean: true,
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js", ".jsx"],
+    extensions: [".js", ".jsx"],
   },
   module: {
     rules: [
       {
-        test: /\.(ts|js)x?$/,
+        test: /\.(js)x?$/,
         exclude: /node_modules/,
         use: [
           {
             loader: "babel-loader",
           },
         ],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)/i,
-        type: "asset/resource",
-        generator: {
-          filename: "assets/img/[hash][ext][query]",
-        },
       },
     ],
   },
