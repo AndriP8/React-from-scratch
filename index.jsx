40bec5c4d639fe13d0a36ae641b99aed4855a9d7
AndriP8 <andrikcoc26@gmail.com>
2022-03-03 16:31:27 +0700

    chore: added styling configuration in webpack

--- index.jsx
+++ index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import reactDom from "react-dom";
+import "./index.css";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello world</h1>
+      {/* menerapkan styling yang ada pada file css */}
+      <h1 className="title">Hello world</h1>
     </div>
   );
 };
