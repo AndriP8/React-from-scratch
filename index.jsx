8d6531106918b007d5960aa0a82d4d0815a566d2
AndriP8 <andrikcoc26@gmail.com>
2022-03-03 20:18:13 +0700

    chore: added assets configuration

--- index.jsx
+++ index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import reactDom from "react-dom";
+import Logo from "./assets/logo.png";
 import "./index.css";
 
 const App = () => {
@@ -7,6 +8,8 @@ const App = () => {
     <div>
       {/* menerapkan styling yang ada pada file css */}
       <h1 className="title">Hello world</h1>
+      {/* menambahkan gambar */}
+      <img src={Logo} alt="logo-react" />
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,13 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/img/[hash][ext][query]",
+        },
+      },
     ],
   },
   plugins: [
