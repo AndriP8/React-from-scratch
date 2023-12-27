8f13779671e603a1584ed764b8cfb1aa64e404bc
Andri Purnomo <andri.adrp@gmail.com>
2023-12-27 19:15:42 +0700

    chore: use pnpm ad update packages

--- index.jsx
+++ index.jsx
@@ -1,17 +1,16 @@
-import React from "react";
-import reactDom from "react-dom";
-import Logo from "./assets/logo.png";
-import "./index.css";
+import React from 'react';
+import reactDom from 'react-dom/client';
+import Logo from './assets/logo.png';
+import './index.css';
 
 const App = () => {
   return (
     <div>
-      {/* menerapkan styling yang ada pada file css */}
       <h1 className="title">Hello world</h1>
-      {/* menambahkan gambar */}
       <img src={Logo} alt="logo-react" />
     </div>
   );
 };
 
-reactDom.render(<App />, document.getElementById("root"));
+const root = reactDom.createRoot(document.getElementById('root'));
+root.render(<App />);
